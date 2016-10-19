e8b3b1d5a0cbd421fd983e38785dc91f8b3a0a58
huangning <huangning@huangning-PC>
2016-10-19 17:31:49 +0800

    add manage.py

--- app/__init__.py
+++ app/__init__.py
@@ -10,10 +10,12 @@ from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from config import config
+
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
--- app/models.py
+++ app/models.py
@@ -1,18 +1,36 @@
 #coding=utf-8
 '''
-Created on 2016Äê10ÔÂ19ÈÕ
+Created on 2016ï¿½ï¿½10ï¿½ï¿½19ï¿½ï¿½
 
 @author: huangning
 '''
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+from flask import current_app
+from . import db
 
-class MyClass(object):
-    '''
-    classdocs
-    '''
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
 
 
-    def __init__(self, params):
-        '''
-        Constructor
-        '''
-        
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    score = db.Column(db.Integer)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    def __repr__(self):
+        return '<User %r>' % self.username
