836329c0340bae909df69fa56404fe1b5d2caf39
huangning <huangning@huangning-PC>
2016-10-21 17:29:08 +0800

    add lost picture

--- app/auth/views.py
+++ app/auth/views.py
@@ -19,8 +19,8 @@ from .forms import LoginForm, RegistrationForm, ChangePasswordForm,\
 def before_request():
     if current_user.is_authenticated:
         current_user.ping()
-        #if not current_user.confirmed and request.endpoint[:5] != 'auth.':
-           #return redirect(url_for('auth.unconfirmed'))
+        if not current_user.confirmed and request.endpoint[:5] != 'auth.':
+           return redirect(url_for('auth.unconfirmed'))
 
 
 @auth.route('/unconfirmed')
