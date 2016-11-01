2e4bdbdee4a00577db71170ad4fcd9d70c80814c
huangning <huangning@huangning-PC>
2016-11-01 11:41:16 +0800

    add display posts post_by followed user in index

--- app/models.py
+++ app/models.py
@@ -190,6 +190,11 @@ class User(UserMixin, db.Model):
         
     def is_followed_by(self, user):
         return self.followers.filter_by(follower_id=user.id).first() is not None
+    
+    @property
+    def followed_posts(self):
+        return Post.query.join(Follow, Follow.followed_id == Post.author_id)\
+            .filter(Follow.follower_id == self.id)
 
 
 class Post(db.Model):
